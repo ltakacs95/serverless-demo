6a7419fc63c8e29ab3fdcb3961cbd05c0e9f50f2
LTakacs <LTakacs@anexia-it.com>
2022-05-20 21:32:51 +0200

    fix: json output

--- helloWorld.js
+++ helloWorld.js
@@ -1,7 +1,10 @@
 exports.helloWorld = (event, context) => {
   console.log('hello serverless world')
   return {
-    status: 202,
+    status: 200,
+    response_headers: {
+      'Content-Type': 'application/json'
+    },
     data: {
       key: 'value'
     }
